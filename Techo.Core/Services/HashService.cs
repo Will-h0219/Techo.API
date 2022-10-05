36d802d759b1bdce2cce05224a395509adbfd48a
Will-h0219 <wherrera0219@gmail.com>
2022-10-05 11:44:17 -0500

    service for hash password

---
+++ Techo.Core/Services/HashService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Techo.Core.Contracts.Services;
+using Techo.Models.Models;
+
+namespace Techo.Core.Services
+{
+    public class HashService : IHashService
+    {
+        public ResultadoHash Hash(string textoPlano)
+        {
+            var sal = new byte[16];
+            using (var random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(sal);
+            }
+
+            return Hash(textoPlano, sal);
+        }
+
+        public ResultadoHash Hash(string textoPlano, byte[] sal)
+        {
+            var llaveDerivada = KeyDerivation.Pbkdf2(password: textoPlano,
+                salt: sal,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 32);
+
+            var hash = Convert.ToBase64String(llaveDerivada);
+
+            return new ResultadoHash()
+            {
+                Hash = hash,
+                Sal = sal
+            };
+        }
+    }
+}
